1ef4f0bf7504d674c7d4530d1822d3e34bf86082
ToniEstarlich <110102891+ToniEstarlich@users.noreply.github.com>
2025-05-29 23:29:07 +0100

    first commit for Pixel

---
+++ products/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    stock = models.IntegerField()
+    size = models.CharField(max_length=10)
+    color = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField(upload_to='product_images/', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
